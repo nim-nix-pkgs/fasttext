a05a32edac1d6bc2a5173708c600ef85312f786f
hugosenari <hugosenari@gmail.com>
2022-03-22 00:32:23 -0300

    chore: re-index ../nimpkgs/f/fasttext

--- master/flake.nix
+++ master/flake.nix
@@ -9,7 +9,7 @@
   
   inputs.src-fasttext-master.flake = false;
   inputs.src-fasttext-master.owner = "bung87";
-  inputs.src-fasttext-master.ref   = "refs/heads/master";
+  inputs.src-fasttext-master.ref   = "master";
   inputs.src-fasttext-master.repo  = "fastText";
   inputs.src-fasttext-master.type  = "github";
   
